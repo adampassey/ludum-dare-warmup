a316bea45a099b8ba7f81c9c32686efc7b7eb071
Adam Passey <adamjpassey@gmail.com>
2015-07-18 09:53:32 -0700

    Camera Controller

---
+++ Assets/Scripts/Local/Camera/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject target;
+    public float speed = 5f;
+
+    private Vector3 moveDirection;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 targetPos = target.transform.position;
+        targetPos.z = transform.position.z;
+
+        moveDirection = targetPos - transform.position;
+	}
+
+    public void FixedUpdate() {
+
+        transform.Translate(moveDirection * speed * Time.deltaTime);
+        
+        moveDirection = Vector3.zero;
+    }
+}
