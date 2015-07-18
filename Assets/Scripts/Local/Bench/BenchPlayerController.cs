cca06e183be98326bb928dba668c7300139833e4
Adam Passey <adamjpassey@gmail.com>
2015-07-18 12:25:06 -0700

    Get some.

--- Assets/Scripts/Local/Bench/BenchPlayerController.cs
+++ Assets/Scripts/Local/Bench/BenchPlayerController.cs
@@ -3,16 +3,23 @@ using System.Collections;
 
 public class BenchPlayerController : MonoBehaviour {
 
+    private Animator animator;
+
+    private static readonly string GET_SOME = "Getting Some";
+
 	// Use this for initialization
 	void Start () {
-	
+        animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
         if (Input.GetMouseButtonDown(0)) {
-            Debug.Log("Get Some!");
+            animator.SetBool(GET_SOME, true);
+        }
+        else {
+            animator.SetBool(GET_SOME, false);
         }
 	}
 }
