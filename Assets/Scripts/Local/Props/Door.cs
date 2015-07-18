cc10f93b8fd4908871ecabcb3a4f8a5ac5693aa5
Adam Passey <adamjpassey@gmail.com>
2015-07-18 09:24:39 -0700

    Adding door to transition scene

---
+++ Assets/Scripts/Local/Props/Door.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+    public void OnTriggerEnter(Collider other) {
+        PlayerController playerController = other.GetComponent<PlayerController>();
+        if (playerController != null) {
+            Debug.Log("Player reached door!");
+        }
+    }
+}
